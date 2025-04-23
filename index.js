f6c9f4ebaebb37f8f55ac0dd909c7e0d488150c3
Viraj Verlekar <viraj.v@turtlemint.com>
2025-04-23 11:25:44 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default class PocMf extends React.PureComponent {
+  render() {
+    return (
+      <View>
+      	<Text>
+      		PocMf
+      	</Text>
+      </View>
+    );
+  }
+}
