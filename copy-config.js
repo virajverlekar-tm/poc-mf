9b22253a6b62c0d91790323672ab838298f3155a
Viraj Verlekar <viraj.v@turtlemint.com>
2025-04-24 13:03:34 +0530

    copy files to assets

---
+++ copy-config.js
@@ -0,0 +1,16 @@
+{
+  "copyFiles": [
+    {
+      "from": "html/user-details.html",
+      "to": "../../android/app/src/main/assets/react-native-poc-mf/html/user-details.html"
+    },
+    {
+      "from": "html/insurance-details.html",
+      "to": "../../android/app/src/main/assets/react-native-poc-mf/html/insurance-details.html"
+    },
+    {
+      "from": "html/insurance-details-pdp.html",
+      "to": "../../android/app/src/main/assets/react-native-poc-mf/html/insurance-details-pdp.html"
+    }
+  ]
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import UserDetails from './html/user-details.html';
-import InsuranceDetails from './html/insurance-details.html';
-import InsuranceDetailsPdp from './html/insurance-details-pdp.html';
+// import UserDetails from './html/user-details.html';
+// import InsuranceDetails from './html/insurance-details.html';
+// import InsuranceDetailsPdp from './html/insurance-details-pdp.html';
 
-export default {"user-details": UserDetails, "insurance-details": InsuranceDetails, "insurance-details-pdp": InsuranceDetailsPdp}
+export default {"user-details": { uri: 'file:///android_asset/react-native-poc-mf/html/user-details.html' }, "insurance-details": { uri: 'file:///android_asset/react-native-poc-mf/html/insurance-details.html' }, "insurance-details-pdp": { uri: 'file:///android_asset/react-native-poc-mf/html/insurance-details-pdp.html' }}
